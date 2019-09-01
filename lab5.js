1dba5bb34979d6afd0ed2d3f35895f4606135207
zhi yong lim <zlim0016@student.monash.edu>
2019-09-02 01:04:21 +1000

    new changes

--- lab5.js
+++ lab5.js
@@ -1,6 +1,22 @@
+//express
 let express= require('express');
 let app= express();
 
+//mongoDB
+let mongodb= require('mongodb');
+let MongoClient= mongodb.MongoClient;
+let url= 'mongodb://localhost:27017/'
+let db;
+MongoClient.connect(url, {useNewUrlParser: true}, function (err, client){
+    if (err){
+        console.log( 'Err  ', err);
+    } else {
+        console.log('Connected successfully to Mongo server');
+        db= client.db('fit2095');
+    }
+})
+
+//bodyParser
 let bodyParser= require('body-parser'); //used to parse the payload of the incoming POST requests. 
 app.use(bodyParser.urlencoded({extended:false})); //allow Express to understand the urlencoded format
 
@@ -12,13 +28,6 @@ app.use(express.static('views'));
 app.use(express.static('css'));
 
 
-let db=[];
-example1={
-    taskName: 'e-Business',
-    taskDue: '12/12/2019',
-    taskDesc: 'Oral Presentation'
-}
-db.push(example1);
 
 app.get('/', function (req, res){
     res.render('index5.html');
@@ -28,16 +37,61 @@ app.get('/addtask', function (req, res){
     res.render('addtask.html');
 })
 
+app.get('/deletetask', function (req, res){
+    res.render('deletetask.html');
+})
+
+app.get('/updatetask', function (req, res){
+    res.render('updatetask.html');
+})
+
 app.get('/listtask', function (req,res){
-    res.render('listtask.html', {task: db});
+    db.collection('week5table').find({}).toArray(function (err, data){
+        res.render('listtask.html', {task: data});
+    })
+    
 })
 
 app.post('/newTask', function (req, res){
-    console.log(req.body)
-    db.push(req.body);
-    res.render('listtask.html', {task: db});
+    let task= req.body;
+    db.collection('week5table').insertOne(task);
+    db.collection('week5table').find({}).toArray(function (err, data){
+        res.render('listtask.html', {task: data});
+    })
     
 })
-//console.log(db);
+
+
+app.post('/deleteTask', function (req, res){
+    let id= req.body._id
+
+    
+    db.collection('week5table').deleteOne({_id: new mongodb.ObjectID(id)}, function (err, obj) {
+        console.log(obj.result);
+    });
+    db.collection('week5table').find({}).toArray(function (err, data){
+        res.render('listtask.html', {task: data});
+    })
+   
+    
+})
+
+app.post('/updateTask', function (req, res){
+    let id= req.body._id;
+    let status= req.body.newStatus;
+    console.log(status,id);
+    db.collection('week5table').updateOne({_id: new mongodb.ObjectID(id)}, {$set: {taskStatus: status}}, { upsert: false},function (err, obj) {
+    });
+
+    db.collection('week5table').deleteMany({taskStatus: 'Complete'}, function (err, obj) {
+        console.log(obj.result);
+    }); //delete all completed tasks
+
+    db.collection('week5table').find({}).toArray(function (err, data){
+        res.render('listtask.html', {task: data});
+    })
+   
+    
+})
 
 app.listen(8080);
